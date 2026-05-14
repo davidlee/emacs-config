f500c20ebb0e3055b0e9fb6bec5aec2c3e4b5b3d
David Lee <dav@davlee.com>
2026-05-15 00:10:23 +1000

    fix: meow-mode init.

--- core/dl-meow.el
+++ core/dl-meow.el
@@ -6,6 +6,7 @@
 (use-package meow
   :config
   :after dl-keymap
+  (setq meow-mode-state-list (append meow-mode-state-list '((vterm-mode . insert))))   
   (meow-setup)
   (meow-global-mode 1))
 
@@ -18,7 +19,6 @@
 ;; +TODO+ binding for:
 ;; vterm-send-escape
 
-(setq meow-mode-state-list (append meow-mode-state-list '((vterm-mode . insert))))
 (defun my-disable-meow-in-vterm ()
   (when (derived-mode-p 'vterm-mode)
     (meow-mode -1)))
